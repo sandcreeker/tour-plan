85ee72059b335a98e24aabf5bfe26af291073d2d
sandcreeker <cheef.trifonov@ya.ru>
2020-12-07 19:11:34 +0300

    create: validation

--- js/main.js
+++ js/main.js
@@ -89,3 +89,29 @@ $(document).ready(function () {
         $('.modal__close')[0].click();
       }
     });
+
+  $('.form').each(function(){
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name"
+        },
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com",
+        },
+      phone: {
+        required: "Please specify your phone",
+        phone: "Your phone must be in the format of +7 (999) 999-99-99",
+        minlength: "Enter the correct number",
+        },
+      subscribe: {
+        required: "We need your email address to contact you",
+        subscribe: "Your email address must be in the format of name@domain.com",
+        },
+      },
+    });
+  });
+
+  $('.modal__phone').mask('+0 (000) 000 00 00');
