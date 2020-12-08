8edb9d08d79a1faf0753d4560d5040c1a6e696df
sandcreeker <cheef.trifonov@ya.ru>
2020-12-08 11:49:47 +0300

    fixed: subscribe validate

--- send.php
+++ send.php
@@ -1,16 +1,15 @@
 <?php
-// Файлы phpmailer
+
 require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
-// Переменные, которые отправляет пользователь
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 $subscribe = $_POST ['subscribe'];
 
-// Формирование самого письма
+
 $title = "Отзыв Best Tour Plan";
 $body = "
 <h2>Пользователь оставил отзыв</h2>
@@ -24,7 +23,6 @@ $body = "
         <h2>Спасибо, что подписались</h2>";
     }
 
-// Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
 try {
     $mail->isSMTP();   
@@ -33,23 +31,22 @@ try {
     // $mail->SMTPDebug = 2;
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
-    // Настройки вашей почты
-    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-    $mail->Username   = 'daniltrifonov367@gmail.com'; // Логин на почте
-    $mail->Password   = 'Lyb-5EH-rLN-ZsA'; // Пароль на почте
+    $mail->Host       = 'smtp.gmail.com'; 
+    $mail->Username   = 'daniltrifonov367@gmail.com';
+    $mail->Password   = 'Lyb-5EH-rLN-ZsA'; 
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
-    $mail->setFrom('daniltrifonov367@gmail.com', 'Данил Трифонов'); // Адрес самой почты и имя отправителя
+    $mail->setFrom('daniltrifonov367@gmail.com', 'Данил Трифонов'); 
 
-    // Получатель письма
+
     $mail->addAddress('cheef.trifonov@yandex.ru');
 
-    // Отправка сообщения
+
     $mail->isHTML(true);
     $mail->Subject = $title;
     $mail->Body = $body;    
 
-// Проверяем отравленность сообщения
+
 if ($mail->send()) {$result = "success";} 
 else {$result = "error";}
 
