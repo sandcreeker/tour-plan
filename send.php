3c8ca4d43ec43885abe487f496a20754da21b86e
sandcreeker <cheef.trifonov@ya.ru>
2020-12-13 12:01:15 +0300

    fixed: booking input

--- send.php
+++ send.php
@@ -8,6 +8,9 @@ $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 $subscribe = $_POST ['subscribe'];
+$bookingName = $_POST ['booking-name'];
+$bookingPhone = $_POST ['booking-phone'];
+$bookingEmail = $_POST ['booking-email'];
 
 
 $title = "Отзыв Best Tour Plan";
@@ -21,6 +24,17 @@ $body = "
         $title = "Подписка оформлена";
         $body = "
         <h2>Спасибо, что подписались</h2>";
+    }
+
+    if ($bookingName & $bookingPhone & $bookingEmail) {
+        $title = "Пользователь забронировал отель";
+        $body = "
+        <h2>Параметры обращения</h2>
+        <b>Имя:</b> $bookingName<br>
+        <b>Телефон:</b> $bookingPhone<br><br>
+        <b>Email:</b> $bookingEmail<br><br>
+        <b>Сообщение:</b><br>$message";
+        
     }
 
 $mail = new PHPMailer\PHPMailer\PHPMailer();
